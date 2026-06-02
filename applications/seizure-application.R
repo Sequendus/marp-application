9c16631c38b2b7079ce800e7a37aa637e343da93
Vanessa Huang <vaness51139@gmail.com>
2026-06-02 16:02:53 +1000

    rerun model averaging and change repo structure

--- applications/seizure-application.R
+++ applications/seizure-application.R
@@ -55,13 +55,13 @@ set.seed(2026)
 # set parameters
 m <- 80 # number of iterations for MLE optimization
 t <- quantile(dat, probs = seq(0.1, 0.9, length.out = 4))
-B <- 99 # number of bootstraps
-BB <- 99 # number of double-bootstrapps
+B <- 300 # number of bootstraps
+BB <- 60 # number of double-bootstrapps
 alpha <- 0.05 # confidence level
 y <- mean(dat) 
 # model_gen <- 2 # specifying the data generating model (if known)
 
-# step one: fitting differnt renewal models
+# step one: fitting different renewal models
 res1 <- marp::poisson_rp(dat,t,y)
 res2 <- marp::gamma_rp(dat,t,m,y)
 res3 <- marp::loglogis_rp(dat,t,m,y)
@@ -82,7 +82,7 @@ ci
 
 # ==== graphs for paper ====
 
-pdf("graphs/loghaz_plot_seizure.pdf")
+pdf("figures/loghaz_plot_seizure.pdf")
 
 # --- Empirical log-hazard ---
 
@@ -127,4 +127,32 @@ legend("topleft",
        bty = "n")
 
 dev.off()
+
+# Save R objects
+saveRDS(res, "results/seizure/res_seizure.rds")
+saveRDS(ci,  "results/seizure/ci_seizure.rds")
+
+# Save CSV summaries
+model_summary <- data.frame(
+  model  = c("Poisson", "Gamma", "LogLogistic", "Weibull", "LogNormal", "BPT"),
+  par1   = res$par1,
+  par2   = c(NA, res$par2[-1]),
+  logL   = res$logL,
+  AIC    = res$AIC,
+  BIC    = res$BIC,
+  weight = res$weights_AIC,
+  mu_hat = res$mu_hat,
+  pr_hat = res$pr_hat
+)
+write.csv(model_summary, "results/seizure/model_summary_seizure.csv", row.names = FALSE)
+
+haz_summary <- data.frame(
+  quantile      = c("10%", "36.7%", "63.3%", "90%"),  # 4 quantiles not 7
+  t             = t,
+  haz_best      = res$haz_best,
+  haz_aic       = res$haz_aic,
+  haz_lower_ma  = ci$student_CI$haz_lower_ma,
+  haz_upper_ma  = ci$student_CI$haz_upper_ma
+)
+write.csv(haz_summary, "results/seizure/haz_summary_seizure.csv", row.names = FALSE)
 
